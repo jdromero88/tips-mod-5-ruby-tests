87e6d22c42718f471642962792f329da0a7a5d19
jdromero88 <github email address>
2020-02-09 16:31:18 -0500

    recurring moment is hard

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -1,30 +1,38 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'pry'
 
 class RecurringMoment
   def initialize(start:, interval:, period:)
     @start = start
     @interval = interval
     @period = period
+    # binding.pry
   end
 
   def match(date)
     current = @start
 
-    while current < date
-      if current == date
+    while current <= date
+      if current >= date
+        # if @period == 'monthly'
+        #   binding.pry
+        # end
         return true
       end
 
       if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
+        # if @interval > 1
+        #   binding.pry
+        # end
         current = current.advance(months: @interval)
+      elsif @period == 'weekly'
+        current = current.advance(weeks: @interval)
       elsif @period == 'daily'
-        current = current.advance(days: @interval)
+        # binding.pry
+        current = current.advance(days: @interval )
       end
     end
-
     return false
   end
 end
